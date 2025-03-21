2f4a466afe19bee9958e93994ebf9611d2f917d2
xXDarkayTberXx <madalolito22@gmail.com>
2025-03-21 20:38:37 +0100

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -7,24 +7,24 @@ export default {
   routes: [
   {
     "renderMode": 2,
-    "route": "/myLoveApp"
+    "route": "/"
   },
   {
     "renderMode": 2,
-    "route": "/myLoveApp/calendar"
+    "route": "/calendar"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/myLoveApp",
-    "route": "/myLoveApp/**"
+    "redirectTo": "/",
+    "route": "/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 39585, hash: 'df89ce3fb3841d8ea3129f7043e6bd106ba08b6cb84987de2572a191cd86ffbb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 33139, hash: 'e00d63da61c648b7ca5bb5c3fb56e75950e4bd1a07862cfdfdc74cb1134ab924', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'myLoveApp/index.html': {size: 52253, hash: 'be3c558cdb5ac74f1935784a7ee39e451d8df1045daba8b4cf336a07b86a2099', text: () => import('./assets-chunks/myLoveApp_index_html.mjs').then(m => m.default)},
-    'myLoveApp/calendar/index.html': {size: 104034, hash: 'f1bce2efed9691fcfb342e31af9d536a3527c4aa6a18cd8b97ed30d51c7128db', text: () => import('./assets-chunks/myLoveApp_calendar_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 39502, hash: '34eb75a3423b4d2f85a75ca3e7046bd1f7fec9de2b8571aee4a39ee571e76840', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 33056, hash: 'd1d3beb34f77c431a2944d1a942722fa93868588601210ae7ecd1369b694998e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'calendar/index.html': {size: 103951, hash: '9e21d98e1714793d1267fb2f6830b530cac50ec467e32b4091c86cb8f0cce443', text: () => import('./assets-chunks/calendar_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 52170, hash: 'b8d5f7a041eb7d6fd33d511b33d53e0068baf63155a0dee249b763fc706a9383', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-KSGJMKDI.css': {size: 11028, hash: 'xkNZEM2rl/E', text: () => import('./assets-chunks/styles-KSGJMKDI_css.mjs').then(m => m.default)}
   },
 };
